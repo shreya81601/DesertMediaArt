7390c62b2e4b23f53b10919c76648269783269bf
shreya81601 <shreya81601@gmail.com>
2022-09-12 01:15:29 +0400

    edits to code

--- Homework Exercises/CodingEx1-RGB-led.py
+++ Homework Exercises/CodingEx1-RGB-led.py
@@ -15,33 +15,39 @@ import neopixel
 from rainbowio import colorwheel
 
 led = neopixel.NeoPixel(board.NEOPIXEL, 1) # assign LED
+secToStay = 5 # How long each part should stay for (in sec)
+startTime = time.monotonic() # get current time
 
+# make function to set controls at the beginning of each part
+def setControls(brghtness, timePeriod):
+    led.brightness = brghtness # set LED brightness
+    global secToStay # changing global variable so that value can be accessed outside function
+    secToStay = timePeriod # How long each part should stay for (in sec)
+    global startTime # changing global variable so that value can be accessed outside function
+    startTime = time.monotonic() # get current time
 
 ################################## PART 1 #####################################
 
 # set controls for part 1
-led.brightness = 0.5 # set LED brightness
-startTime = time.monotonic() # get current time
-secToBlink = 6 # How long to blink for (in sec)
+setControls(0.5, 6)
 
 # led for part 1 - red ambulance light
-while (time.monotonic() - startTime) < secToBlink:
+while (time.monotonic() - startTime) < secToStay:
     led[0] = (255, 0, 0)
     time.sleep(0.11)
     led[0] = (0, 0, 0)
     time.sleep(0.11)
 
-time.sleep(3) # wait time before next section starts
+time.sleep(2.5) # wait time before next section starts
 
 
 ################################## PART 2 #####################################
 
 # set controls for part 2
-led.brightness = 1 # set LED brightness
-startTime = time.monotonic() # get current time
-secToStay = 4 # How long the white light should stay for (in sec)
+setControls(1, 6)
 
 # make the white light appear slowly
+# -> removed because OT lights come on in a split of a second
 # i = 0
 # for i in range(100):
 #     led.brightness = i/100
@@ -50,7 +56,7 @@ secToStay = 4 # How long the white light should stay for (in sec)
     # time.sleep(0.1)
 
 # led for part 2 - constant OT light
-while (time.monotonic() - startTime) < secToBlink:
+while (time.monotonic() - startTime) < secToStay:
     led[0] = (255, 255, 255)
 led[0] = (0, 0, 0)
 
@@ -60,16 +66,14 @@ time.sleep(2.5) # wait time before next section starts
 ################################## PART 3 #####################################
 
 # set controls for part 3
-led.brightness = 0.3 # set LED brightness
-startTime = time.monotonic() # get current time
-secToBlink = 12 # How long to blink for (in sec)
+setControls(0.5, 12)
 
 # led for part 3 - the wait time - yellow blinking light
-while (time.monotonic() - startTime) < secToBlink:
+while (time.monotonic() - startTime) < secToStay:
 
     # fade in the yellow light
     i = 0
-    for i in range(100):
+    for i in range(50):
         led.brightness = i/100
         led[0] = (255, 255, 0)
     led[0] = (255, 255, 0)
@@ -77,8 +81,8 @@ while (time.monotonic() - startTime) < secToBlink:
 
     # fade out the yellow light
     i = 0
-    for i in range(100):
-        led.brightness = 1-i/100
+    for i in range(50):
+        led.brightness = 0.5-i/100
         led[0] = (255, 255, 0)
     led[0] = (0, 0, 0)
     time.sleep(0.9)
@@ -89,31 +93,10 @@ time.sleep(1.5) # wait time before next section starts
 ################################## PART 4 #####################################
 
 # set controls for part 4
-led.brightness = 0.5 # set LED brightness
-startTime = time.monotonic() # get current time
-secToStay = 3 # How long the white light should stay for (in sec)
-
-# make the green light appear slowly
-# i = 0
-# for i in range(100):
-#     led.brightness = i/100
-#     led[0] = (255, 255, 255)
-#     i = i+5
-    # time.sleep(0.1)
+setControls(0.3, 4)
 
 # led for part 3 - green ok light
-while (time.monotonic() - startTime) < secToBlink:
+while (time.monotonic() - startTime) < secToStay:
     led[0] = (0, 255, 0)
 led[0] = (0, 0, 0)
-
-
-################################## PART 5 #####################################
-
-# rainbow color light for recovery state --> deleted
-# i = 0
-# while True:
-#     i = (i + 1) % 256  # run from 0 to 255
-#     led.fill(colorwheel(i))
-#     time.sleep(0.025)
-
 
